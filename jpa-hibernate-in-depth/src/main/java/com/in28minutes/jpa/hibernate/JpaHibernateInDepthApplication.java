061a43d55c553bff3323724bf7c7ae487466b507
Harry <shaikmohammadibrahim311@gmail.com>
2024-05-05 12:27:43 +0530

    CourseEntity

---
+++ jpa-hibernate-in-depth/src/main/java/com/in28minutes/jpa/hibernate/JpaHibernateInDepthApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateInDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateInDepthApplication.class, args);
+	}
+
+}
---
+++ jpa-hibernate-in-depth/src/main/java/com/in28minutes/jpa/hibernate/entity/Course.java
@@ -0,0 +1,5 @@
+package com.in28minutes.jpa.hibernate.entity;
+
+public class Course {
+
+}
